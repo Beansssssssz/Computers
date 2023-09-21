4b57558accea4156d39c99fedd2132c4172e661f
ariel_laptop <s32411@nomishemer.ort.org.il>
2023-09-21 14:18:17 +0300

    Added main function and SetState function to traffic light

--- TraficLights/TraficLights/TraficLight.cs
+++ TraficLights/TraficLights/TraficLight.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Shapes;
 
 namespace TraficLights
@@ -15,12 +17,42 @@ namespace TraficLights
             Yellow,
             Green
         }
-        private TraficLightState state;//the state that a light can be in
+        private TraficLightState _state;//the state that a light can be in
 
-        private Ellipse redElp;
-        private Ellipse greenElp;
-        private Ellipse yellowElp;
+        private Ellipse _elpRed;
+        private Ellipse _elpGreen;
+        private Ellipse _elpYellow;
 
         private bool _isAuto;
+
+        public TraficLight(Ellipse elpRed ,Ellipse elpGreen, Ellipse elpYellow)
+        {
+            _elpRed = elpRed;
+            _elpGreen = elpGreen;
+            _elpYellow = elpYellow;
+            _state = TraficLightState.Red;
+        }
+
+        /// <summary>
+        /// the function movest the traffic light to its necct state in a infinite loop
+        /// </summary>
+        public void SetState()
+        {
+            switch (_state)
+            {
+                case TraficLightState.Green:
+                    _state = TraficLightState.Red;
+                    _elpRed.Fill = new SolidColorBrush(Colors.Red);
+                    break;
+                case TraficLightState.Red:
+                    _state = TraficLightState.Yellow;
+                    _elpRed.Fill = new SolidColorBrush(Colors.Yellow);
+                    break;
+                default:
+                    _state = TraficLightState.Green;
+                    _elpRed.Fill = new SolidColorBrush(Colors.Green);
+                    break;
+            }
+        }
     }
 }
