0a0e7ed822e230024431e8a267ed15cfa08a0910
ariel_laptop <s32411@nomishemer.ort.org.il>
2023-09-21 09:39:53 +0300

    added c# files for objects

---
+++ TraficLights/TraficLights/TraficLight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraficLights
+{
+    class TraficLight
+    {
+        public enum TraficLightState
+        {
+            Red,
+            Yellow,
+            Green
+        }
+        private TraficLightState state;//the state that a light can be in
+    }
+}
