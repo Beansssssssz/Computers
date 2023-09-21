579fc31c48dde5d2d6920675385e577f88702c0c
ariel_laptop <s32411@nomishemer.ort.org.il>
2023-09-21 14:04:39 +0300

    fixed a bug where there waas a missing file and made things more pretty

--- TraficLights/TraficLights/TraficLight.cs
+++ TraficLights/TraficLights/TraficLight.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Shapes;
 
 namespace TraficLights
 {
@@ -15,5 +16,11 @@ namespace TraficLights
             Green
         }
         private TraficLightState state;//the state that a light can be in
+
+        private Ellipse redElp;
+        private Ellipse greenElp;
+        private Ellipse yellowElp;
+
+        private bool _isAuto;
     }
 }
