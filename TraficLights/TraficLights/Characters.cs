c1de3c0c53a2f0bfe5fc428946377181633337f6
Ariel hay <rylhy9502@gmail.com>
2023-10-24 13:51:18 +0300

    added the Charactters class

--- TraficLights/TraficLights/Characters.cs
+++ TraficLights/TraficLights/Characters.cs
@@ -1,13 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace TraficLights
 {
-    public abstract class Characters
+  public abstract class Characters
+  {
+    public enum StateType {// the current state of the character
+      standing,
+      ready,
+      going
+    }
+
+    private Image _imageAnimal;
+    protected BitmapImage _bitmapImage;
+    protected StateType _state;
+
+    public Characters(Image imageAnimale)
+    {
+      _state = StateType.standing;
+      _imageAnimal = imageAnimale;
+      _bitmapImage = new BitmapImage();
+      _imageAnimal.Source = _bitmapImage;
+      MatchGifToState();
+    }
+
+    protected virtual void MatchGifToState()
     {
 
     }
+
+  }
 }
