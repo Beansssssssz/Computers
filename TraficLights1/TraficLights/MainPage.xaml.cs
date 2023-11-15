100d758fef96ad0ebaf039c5d96b53819b95ba87
ariel_laptop <s32411@nomishemer.ort.org.il>
2023-11-16 00:04:50 +0200

    ***

--- TraficLights1/TraficLights/MainPage.xaml.cs
+++ TraficLights1/TraficLights/MainPage.xaml.cs
@@ -22,9 +22,31 @@ namespace TraficLights
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public TraficLight _traficLight;
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void btnManual_Click(object sender, RoutedEventArgs e)
+        {
+            _traficLight.SetState();
+        }
+
+        private void btnAuto_Click(object sender, RoutedEventArgs e)
+        {
+            _traficLight.IsAuto =! _traficLight.IsAuto;
+            if (_traficLight.IsAuto)
+                btnAuto.Content = "Stop";
+            else
+                btnAuto.Content = "Auto";
+            btnManual.IsEnabled =! _traficLight.IsAuto;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            _traficLight = new TraficLight(RedElp, YellowElp, GreenElp);
         }
     }
 }
